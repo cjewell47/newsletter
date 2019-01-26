7cc1d6846ba890df73b917c0b3b3cc314eeb4059
cjewell47 <charles.jewell47@gmail.com>
2019-01-26 17:07:23 +0000

    thanks page done

--- newsletter-page/src/components/Thanks.js
+++ newsletter-page/src/components/Thanks.js
@@ -2,10 +2,13 @@ import React from "react";
 
 const Thanks = () => {
   return (
-      <div className="Thanks">
-      <h1>Thanks</h1>
+    <div className="Thanks">
+      <div className="wrapper">
+        <img src="./images/butterfly.png" alt="Butterfly" />
+        <h1> <span className="white">Success!</span><br />Thanks for signing up to our newsletter</h1>
       </div>
-  )
+    </div>
+  );
 };
 
 export default Thanks;
